1390e3f2ea529c765d92aa23ff62e2388a254918
U-nity <728501875@qq.com>
2016-09-28 19:51:03 +0800

    Initial commit

---
+++ app/src/main/java/bistu/rookie/u_nity/androidproject_27/MainActivity.java
@@ -0,0 +1,28 @@
+package bistu.rookie.u_nity.androidproject_27;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.SendButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("android.intent.action.MY_BROADCAST");
+                intent.putExtra("message", "This is a broadcast message");
+                sendBroadcast(intent);
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/bistu/rookie/u_nity/androidproject_27/MyReceiver.java
@@ -0,0 +1,21 @@
+package bistu.rookie.u_nity.androidproject_27;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        String message = intent.getStringExtra("message");
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+}
